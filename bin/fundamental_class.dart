d0496335efcd2fdf3149e471a911503c6956ad48
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-10-03 09:42:27 +0700

    Update nambah type mobil dan harga mobil lainnya

--- bin/fundamental_class.dart
+++ bin/fundamental_class.dart
@@ -15,4 +15,6 @@ var avanza = Mobil('Toyota', 2020, 300000000);
 var xenia = Mobil('Daihatsu', 2020, 290000000);
 var ertiga = Mobil('Suzuki', 2021, 250000000);
 avanza.info();
+xenia.info();
+ertiga.info();
 }
